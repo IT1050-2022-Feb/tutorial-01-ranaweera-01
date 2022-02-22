53fc122da0cb01306f22f8153bfda6acf3b9b85d
it21260056 <91587282+it21260056@users.noreply.github.com>
2022-02-22 06:40:31 +0000

    tutorial 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  
+
+  int number1, number2;
+  int sum;
+  float avg;
+
+  printf("Enter mark 1:");
+  scanf("%d", &number1);
+  printf("Enter mark 2:");
+  scanf("%d", &number2);
+  sum = number1 + number2;
+  avg = sum /2;
+  printf("Average is %.2f", avg);
   return 0;
 }
 
